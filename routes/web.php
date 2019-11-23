69fea8eab0284e0367fdcbea8ed3a617860738b5
John Edisson Ortiz <john.ortiz@placetopay.com>
2019-11-23 09:34:11 -0500

    Added routes and controller to collaborators

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('collaborators', 'CollaboratorController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
