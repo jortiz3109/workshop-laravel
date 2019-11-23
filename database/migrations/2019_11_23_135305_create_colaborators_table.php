88b35270f2c5ec52ab173798cb0da66b5e6ddf01
John Edisson Ortiz <john.ortiz@placetopay.com>
2019-11-23 09:07:03 -0500

    Added base migrations

---
+++ database/migrations/2019_11_23_135305_create_colaborators_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateColaboratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colaborators', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 200);
+            $table->string('email', 90)->unique();
+            $table->unsignedInteger('city_id');
+            $table->unsignedInteger('role_id');
+            $table->timestamp('disabled_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colaborators');
+    }
+}
