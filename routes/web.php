b46af63c63a558341ad0e2e9c1dd0c83455a3dfe
John Edisson Ortiz <john.ortiz@placetopay.com>
2019-11-25 09:33:36 -0500

    Protected collaborators and home routes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('collaborators', 'CollaboratorController');
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function (){
+    Route::resource('collaborators', 'CollaboratorController');
+    Route::get('home', 'HomeController@index')->name('home');
+});
