258cc341da5443d1fab025e41e50a34955b8c59f
John Edisson Ortiz <john.ortiz@placetopay.com>
2019-11-25 11:57:24 -0500

    Added new assets to global template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -78,7 +71,9 @@
             @yield('content')
         </main>
     </div>
+    <script src="{{ asset(mix('js/manifest.js')) }}"></script>
+    <script src="{{ asset(mix('js/vendor.js')) }}"></script>
     <script src="{{ asset(mix('js/app.js')) }}"></script>
-    <script src="{{ asset(mix('js/delete-modal.js')) }}"></script>
+    @stack('scripts')
 </body>
 </html>
