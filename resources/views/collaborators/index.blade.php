1437c0d285ae08a9159e34ca5779d9d2f3b67c6d
John Edisson Ortiz <john.ortiz@placetopay.com>
2019-11-25 11:57:43 -0500

    Added changes to collaborators index

--- resources/views/collaborators/index.blade.php
+++ resources/views/collaborators/index.blade.php
@@ -10,17 +10,17 @@
         <div class="table-responsive-lg">
             <table class="table table-hover">
                 <thead>
-                <tr>
-                    <th>{{ __('Name') }}</th>
-                    <th>{{ __('Email') }}</th>
-                    <th>{{ __('City') }}</th>
-                    <th>{{ __('Role') }}</th>
-                    <th>{{ __('Status') }}</th>
-                    <th class="text-right"></th>
-                </tr>
+                    <tr>
+                        <th>{{ __('Name') }}</th>
+                        <th>{{ __('Email') }}</th>
+                        <th>{{ __('City') }}</th>
+                        <th>{{ __('Role') }}</th>
+                        <th>{{ __('Status') }}</th>
+                        <th class="text-right"></th>
+                    </tr>
                 </thead>
                 <tbody>
-                @each('collaborators.index.__row', $collaborators, 'collaborator', 'partials.__empty')
+                    @each('collaborators.index.__row', $collaborators, 'collaborator', 'partials.__empty')
                 </tbody>
             </table>
         </div>
@@ -28,4 +28,7 @@
 @endsection
 @push('modals')
     @include('partials.__confirm_delete_modal')
+@endpush
+@push('scripts')
+    <script src="{{ asset(mix('js/delete-modal.js')) }}"></script>
 @endpush
